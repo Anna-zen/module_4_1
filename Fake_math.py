aa78bfda0d60a71e3f9356926f2b3bf8cae75140
Anna Zen <annadavidson7777@gmail.com>
2024-10-12 22:13:35 +0300

    Add files via upload

---
+++ Fake_math.py
@@ -0,0 +1,13 @@
+
+def divide (first, second) :
+    #print (' Делим ', first, ' на ', second)
+    if second != 0 :
+        result = first/second
+    #    print('Резудьтат равен ', result)
+        return result
+
+    else:
+        return "Ошибка!"
+    #    print ('Ошибка, на ноль делить нельзя!')
+
+
